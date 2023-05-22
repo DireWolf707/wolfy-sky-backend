1d2c829bee530704c161927b3ee3aca947d7b824
DireWolf707 <rahulsood707@gmail.com>
2023-05-23 00:54:58 +0530

    added media input validator

--- src/validators/index.js
+++ src/validators/index.js
@@ -3,6 +3,14 @@ import { AppError } from "../utils"
 const imageMimeTypes = ["image/jpeg", "image/png"]
 export const imageInput = (image) => {
   if (!image) throw new AppError("no image file uploaded")
-  if (!imageMimeTypes.includes(image.mimetype)) throw new AppError("Only .jpg, .jpeg and .png formats are supported")
+  if (!imageMimeTypes.includes(image.mimetype)) throw new AppError("only .jpg, .jpeg and .png formats are supported")
   return image
+}
+
+export const mediaInput = (media) => {
+  if (!media) throw new AppError("no media file uploaded")
+  if (imageMimeTypes.includes(media.mimetype)) return media
+  if (media.mimetype.startsWith("video/")) return media
+
+  throw new AppError("un-supported media file uploaded")
 }
