c05465186e4a46a9dccb947a883017a690927824
DireWolf707 <rahulsood707@gmail.com>
2023-05-24 01:49:17 +0530

    added schema in db client

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,10 +1,12 @@
 import postgres from "postgres"
 import { drizzle } from "drizzle-orm/postgres-js"
+import * as schema from "../drizzle/schema"
 
 const queryClient = postgres(process.env.DATABASE_POOL_URL, {
   ssl: "require",
   debug: Boolean(process.env.DEBUG || false),
 })
-const db = drizzle(queryClient)
+
+const db = drizzle(queryClient, { schema })
 
 export default db
