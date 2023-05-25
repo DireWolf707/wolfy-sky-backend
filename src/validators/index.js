462a852e872af0bb33a977225d487118d0f5ac2d
DireWolf707 <rahulsood707@gmail.com>
2023-05-25 20:21:07 +0530

    modified to returen media type

--- src/validators/index.js
+++ src/validators/index.js
@@ -9,8 +9,8 @@ export const imageInput = (image) => {
 
 export const mediaInput = (media) => {
   if (!media) throw new AppError("no media file uploaded")
-  if (imageMimeTypes.includes(media.mimetype)) return media
-  if (media.mimetype.startsWith("video/")) return media
+  if (imageMimeTypes.includes(media.mimetype)) return [media, "img"]
+  if (media.mimetype.startsWith("video/")) return [media, "vid"]
 
   throw new AppError("un-supported media file uploaded")
 }
