6c55f250714b7fbae5108f727d48f35aef0498e3
DireWolf707 <rahulsood707@gmail.com>
2023-05-25 18:14:50 +0530

    updated sqls

--- src/controllers/twitterController.js
+++ src/controllers/twitterController.js
@@ -1,39 +1,31 @@
 import { catchAsync } from "../utils"
 import { db, storage } from "../configs"
-import {
-  userT,
-  followT,
-  tweetT,
-  likeT,
-  notificationT,
-  tweetSchema,
-  arrayAgg,
-  ilike,
-  eq,
-  sql,
-  alias,
-  ne,
-  and,
-  desc,
-} from "../drizzle/schema"
+import { userT, followT, tweetT, likeT, notificationT, tweetSchema, arrayAggOrder, arrayAgg, alias, commentTweetT } from "../drizzle/schema"
+import { eq, ne, ilike, and, sql, desc } from "drizzle-orm"
 import { mediaInput } from "../validators"
 import { io } from "../socket"
 
 export const getFeed = catchAsync(async (req, res) => {
   const { id } = req.user
 
-  const parentTweetT = alias(tweetT, "parentTweetT")
+  const parentTweetT = alias(tweetT, "ParentTweet")
+  const parentTweetUserT = alias(tweetT, "ParentTweetUser")
   const feed = await db
     .select({
+      ...tweetT,
       user: userT,
-      tweet: tweetT,
-      parentTweet: parentTweetT,
+      isLiked: likeT,
+      parentTweet: {
+        ...parentTweetT,
+        user: parentTweetUserT,
+      },
     })
     .from(followT)
-    .where(eq(followT.from, id))
-    .leftJoin(tweetT, eq(followT.to, tweetT.userId))
-    .leftJoin(parentTweetT, eq(tweetT.parentTweetId, parentTweetT.id))
+    .innerJoin(tweetT, and(eq(id, followT.from), eq(followT.to, tweetT.userId)))
     .leftJoin(userT, eq(tweetT.userId, userT.id))
+    .leftJoin(parentTweetT, eq(tweetT.parentTweetId, parentTweetT.id))
+    .leftJoin(parentTweetUserT, eq(parentTweetT.userId, parentTweetUserT.id))
+    .leftJoin(likeT, and(eq(tweetT.id, likeT.tweetId), eq(likeT.userId, id)))
     .orderBy(desc(tweetT.createdAt))
 
   res.json({ data: feed })
@@ -42,66 +34,86 @@ export const getFeed = catchAsync(async (req, res) => {
 export const getFollowRecomendations = catchAsync(async (req, res) => {
   const { id } = req.user
 
-  const followingOfFollowingT = alias(followT, "followingOfFollowing")
+  const f2fT = alias(followT, "followingToFollowing")
   const whoToFollow = await db
     .select({ ...userT })
     .from(followT)
-    .where(eq(followT.from, id))
-    .leftJoin(followingOfFollowingT, eq(followT.to, followingOfFollowingT.from))
-    .leftJoin(userT, eq(followingOfFollowingT.to, userT.id))
-    .groupBy(userT.id) // to get distinct values
+    .innerJoin(f2fT, and(eq(id, followT.from), eq(followT.to, f2fT.from), ne(f2fT.to, id)))
+    .leftJoin(userT, eq(f2fT.to, userT.id))
+    .groupBy(userT.id, f2fT.createdAt) // to get distinct users
+    .orderBy(desc(f2fT.createdAt))
     .limit(4)
 
   res.json({ data: whoToFollow })
 })
 
 export const getTweet = catchAsync(async (req, res) => {
+  const { id } = req.user
   const { tweetId } = req.params
 
-  const commentT = alias(tweetT, "commentT")
   const [tweet] = await db
-    .select({
-      ...tweetT,
-      comments: arrayAgg(commentT, commentT.id),
-    })
+    .select({ ...tweetT, user: userT, isLiked: likeT })
     .from(tweetT)
-    .where(eq(tweetT.id, tweetId))
-    .leftJoin(commentT, eq(tweetT.id, commentT.parentTweetId))
-    .groupBy(tweetT.id)
-    .orderBy(desc(commentT.createdAt))
-
-  // TODO: include tweet user and comments user
+    .innerJoin(userT, and(eq(tweetId, tweetT.id), eq(tweetT.userId, userT.id)))
+    .leftJoin(likeT, and(eq(tweetT.id, likeT.tweetId), eq(likeT.userId, id)))
 
   res.json({ data: tweet })
 })
 
-export const getPublicProfile = catchAsync(async (req, res) => {
-  const { userId } = req.params
+export const getComments = catchAsync(async (req, res) => {
+  const { id } = req.user
+  const { tweetId } = req.params
 
-  const [profile] = await db
-    .select({
-      user: userT,
-      tweets: arrayAgg(tweetT, tweetT.id),
-      followers: sql`(select count(*) from ${followT} where ${followT.to} = ${userId})`,
-      following: sql`(select count(*) from ${followT} where ${followT.from} = ${userId})`,
-    })
-    .from(userT)
-    .where(eq(userT.id, userId))
-    .leftJoin(tweetT, eq(userT.id, tweetT.userId))
-    .groupBy(userT.id)
+  const comments = await db
+    .select({ ...tweetT, user: userT, isLiked: likeT })
+    .from(tweetT)
+    .innerJoin(userT, and(eq(tweetId, tweetT.parentTweetId), eq(tweetT.userId, userT.id)))
+    .leftJoin(likeT, and(eq(tweetT.id, likeT.tweetId), eq(likeT.userId, id)))
     .orderBy(desc(tweetT.createdAt))
 
-  res.json({ data: profile })
+  res.json({ data: comments })
+})
+
+export const getPublicProfile = catchAsync(async (req, res) => {
+  const { id } = req.user
+  const { userId } = req.params
+
+  const userQ = db
+    .select({
+      ...userT,
+      followers: sql`(select count(*) from ${followT} where ${followT.to} = ${userId})`,
+      following: sql`(select count(*) from ${followT} where ${followT.from} = ${userId})`,
+      isFollowed: sql`(select count(*) from ${followT} where ${followT.from} = ${id} and ${followT.to} = ${userId})`,
+    })
+    .from(userT)
+    .where(eq(userId, userT.id))
+
+  const tweetsQ = db
+    .select({
+      ...tweetT,
+      user: userT,
+      isLiked: likeT,
+    })
+    .from(tweetT)
+    .innerJoin(userT, and(eq(userId, tweetT.userId), eq(tweetT.userId, userT.id)))
+    .leftJoin(likeT, and(eq(tweetT.id, likeT.tweetId), eq(likeT.userId, id)))
+    .orderBy(desc(tweetT.createdAt))
+
+  const [[user], tweets] = await Promise.all([userQ, tweetsQ])
+
+  res.json({ data: { ...user, tweets } })
 })
 
 export const getNotifications = catchAsync(async (req, res) => {
   const { id } = req.user
 
   const notifications = await db
-    .select()
+    .select({
+      ...notificationT,
+      user: userT,
+    })
     .from(notificationT)
-    .where(eq(notificationT.to, id))
-    .leftJoin(userT, eq(notificationT.from, userT.id))
+    .innerJoin(userT, and(eq(id, notificationT.to), eq(notificationT.from, userT.id)))
     .orderBy(desc(notificationT.createdAt))
 
   res.json({ data: notifications })
@@ -111,20 +123,24 @@ export const search = catchAsync(async (req, res) => {
   const { id } = req.user
   const { q } = req.query
 
-  const result = await db
+  const userSq = db
     .select()
     .from(userT)
     .where(and(ne(userT.id, id), ilike(userT.username, `%${q}%`)))
+    .as("user")
+
+  const result = await db
+    .select()
+    .from(userSq)
+    .leftJoin(followT, and(eq(id, followT.from), eq(followT.to, userSq.id)))
 
   res.json({ data: result })
 })
 
 export const createTweet = catchAsync(async (req, res) => {
   const { id } = req.user
-  const { tweetId = null } = req.params
 
-  const data = tweetSchema.parse(req.body)
-
+  const data = tweetSchema.parse({ ...req.body, userId: id })
   const [tweet] = await db.insert(tweetT).values(data).returning()
 
   res.json({ data: tweet })
