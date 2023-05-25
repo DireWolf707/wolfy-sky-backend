6681220d7637651329e70dc948d2f2a4379a8c4c
DireWolf707 <rahulsood707@gmail.com>
2023-05-25 18:14:56 +0530

    added get comments

--- src/routers/twitterRouter.js
+++ src/routers/twitterRouter.js
@@ -4,6 +4,7 @@ import {
   getNotifications,
   getPublicProfile,
   getTweet,
+  getComments,
   search,
   createTweet,
   like,
@@ -25,7 +26,7 @@ router.get("/profile/:userId", getPublicProfile)
 router.route("/profile/:userId/follow").post(follow).delete(unfollow)
 router.post("/tweet", createTweet)
 router.get("/tweet/:tweetId", getTweet)
-router.post("/tweet/:tweetId/reply", createTweet)
+router.get("/tweet/:tweetId/comments", getComments)
 router.route("/tweet/:tweetId/like").post(like).delete(unlike)
 
 export default router
