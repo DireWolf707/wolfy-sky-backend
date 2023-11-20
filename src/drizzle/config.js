742d88ab4776dcdc552ab9b871806bc4c816e820
Rahul <rahulsood707@gmail.com>
2023-11-20 23:24:12 +0530

    added drizzle studio

--- src/drizzle/config.js
+++ src/drizzle/config.js
@@ -3,5 +3,8 @@ import "../configs/dotenv"
 export default {
   schema: "src/drizzle/schema.js",
   out: "src/drizzle/migrations",
-  connectionString: process.env.DATABASE_DIRECT_URL, // for future when pg push is available
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.DATABASE_DIRECT_URL,
+  }
 }
